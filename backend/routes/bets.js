777b2a401569ade746a4de03ac7353dd4957ce91
Mario <mario.lopez.batres@gmail.com>
2019-09-04 00:15:56 +0200

    Create common multer middleware for storing images in different folders

--- backend/routes/bets.js
+++ backend/routes/bets.js
@@ -2,39 +2,14 @@ const express = require("express");
 const multer = require("multer");
 
 const BetController = require("../controllers/bets");
-
-const router = express.Router();
+const extractFile = require("../middleware/file")
 const checkAuth = require('../middleware/check-auth');
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
-}
+const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images/bets");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  }
-});
+router.post("", checkAuth, extractFile, BetController.createBet);
 
-router.post("", checkAuth, multer({
-  storage: storage
-}).single("image"), BetController.createBet);
-
-router.put("/:id", checkAuth, multer({
-  storage: storage
-}).single("image"), BetController.updateBet);
+router.put("/:id", checkAuth, extractFile, BetController.updateBet);
 
 router.get('', BetController.getBets);
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,39 +1,12 @@
 const express = require("express");
-const UserController = require("../controllers/user")
+const UserController = require("../controllers/user");
+const extractFile = require("../middleware/file")
 const router = express.Router();
-
-const multer = require("multer");
-
-
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images/users/");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  }
-});
-
 
 /**
  * Store a new user within an encypted password
  */
-router.post("/signup", multer({
-  storage: storage
-}).single("image"), UserController.createUser);
+router.post("/signup", extractFile, UserController.createUser);
 
 router.post("/login", UserController.userLogin);
 
